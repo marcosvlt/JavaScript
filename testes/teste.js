3dcf8d4e43005c633db41416b7d3696e9308e0cd
marcosvlt <msr.airborne@gmail.com>
2024-09-23 21:55:39 -0300

    testes

---
+++ testes/teste.js
@@ -0,0 +1,10 @@
+// Used for testing with go live in vs code for debugging
+
+
+let idade = prompt('qualsua idade?')
+
+if (idade >= 18) {
+    alert('Pode tirar a habilitação!')    
+} else {
+    alert('Não pode tirar a habilitação!')
+}
