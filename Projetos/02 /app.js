893182865569230b4f5c9ddf5112dda9538cc31f
marcosvlt <msr.airborne@gmail.com>
2024-09-29 19:55:31 -0300

    refactor with function

--- Projetos/02 /app.js
+++ Projetos/02 /app.js
@@ -1,8 +1,18 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Jogo do número secreto';
+// let titulo = document.querySelector('h1');
+// titulo.innerHTML = 'Jogo do número secreto';
 
-let paragrafo  = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+// let paragrafo  = document.querySelector('p');
+// paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+
+function exibirTexto(tag, texto){
+
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+
+};
+
+exibirTexto('h1', 'Jogo do número secreto');
+exibirTexto('p', 'Escolha um número entre 1 e 10');
 
 function verificarChute(params) {
 
